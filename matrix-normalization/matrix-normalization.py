af38ef4b409be6acce24beacbfdc7b0b36c837bc
Olamilekan Akintilebo <94446638+hayohtee@users.noreply.github.com>
2026-07-30 23:50:29 +0100

    Solve matrix-normalization

--- matrix-normalization/matrix-normalization.py
+++ matrix-normalization/matrix-normalization.py
@@ -9,33 +9,22 @@ def matrix_normalization(matrix, axis=None, norm_type='l2'):
 
     if axis not in (None, 0, 1):
         return None
-    
+
     if norm_type not in ("l2", "l1", "max"):
         return None
 
     if A.ndim != 2:
         return None
 
-    norm = None
-
+    norms = None
     match norm_type:
         case "l2":
-            norm = np.sqrt(np.sum(A ** 2, axis=axis))
+            norms = np.sqrt(np.sum(A ** 2, axis=axis, keepdims=True))
         case "l1":
-            norm = np.sum(np.abs(A), axis=axis)
+            norms = np.sum(np.abs(A), axis=axis, keepdims=True)
         case "max":
-            norm = np.max(A, axis=axis)
+            norms = np.max(A, axis=axis, keepdims=True)
 
-    match axis:
-        case 0:
-            for i in range(A.shape[1]):
-                norm_i = norm[i]
-                A[:, i] = 0 if norm_i == 0 else A[:, i] / norm_i
-        case 1:
-            for i in range(A.shape[0]):
-                norm_i = norm[i]
-                A[i, :] = 0 if norm_i == 0 else A[i, :] / norm_i
-        case None:
-            A = 0 if norm == 0 else A / norm
+    norms = np.where(norms == 0, 1, norms)
 
-    return A
+    return A / norms
